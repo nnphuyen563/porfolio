a9a27a714450576cba4b6722c27d3c48a7efb9bc
Nguyễn Ngọc Phương Uyên <nnphuyen563@gmail.com>
2024-04-07 11:18:30 +0700

    fix: Image caption

--- utils/markdown.js
+++ utils/markdown.js
@@ -1,5 +1,10 @@
 const markdownIt = require('markdown-it')
 const markdownItAnchor = require('markdown-it-anchor')
+const markdownItEleventyImg = require("markdown-it-eleventy-img");
+const path = require('path');
+
+const PREFIX = 'Eleventy-Markdown-it'
+const LOG_PREFIX = `[\x1b[34m${PREFIX}\x1b[0m]`
 
 const anchorSlugify = (s) =>
     encodeURIComponent(
@@ -23,6 +28,20 @@ const markdown = markdownIt({
     permalinkAttrs: () => ({ 'aria-hidden': true }),
     level: 2,
     slugify: anchorSlugify
-})
+}).use(markdownItEleventyImg, {
+    resolvePath: (filepath, env) => path.join(path.dirname(env.page.inputPath), filepath),
+    imgOptions: {
+        urlPath: "/",
+        format: ["auto"]
+    },
+    renderImage(image, attributes) {
+        const [ src, attrs ] = attributes;
+        const img_src = src.split("\\").pop();
+
+        const imageMarkup = `<img src="${img_src}" alt="${attrs.alt}" title="${attrs.title}">`;
+    
+        return `<figure class="markdown-image">${imageMarkup}${attrs.title ? `<figcaption>${attrs.title}</figcaption>` : ""}</figure>`;
+    }
+});
 
 module.exports = markdown
